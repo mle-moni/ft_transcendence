b75e6fcc8b3c11c7569700f7ab6818b65a15e6f8
LE MONIES DE SAGAZAN Mayeul <m.lemoniesdesagazan@gmail.com>
2020-10-28 15:07:03 +0100

    auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+
+  get 'home/index'
+  root "home#index"
+
+  # sign in route:
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+
+  # sign out route:
+  devise_scope :user do
+    delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session_path
+  end
+
+  resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
