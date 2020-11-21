cf40a1c2bbbe10a17c1d20e1e0bc8e07f9c1067c
LE MONIES DE SAGAZAN Mayeul <m.lemoniesdesagazan@gmail.com>
2020-11-21 12:29:19 +0100

    re render when no attr changed but a new model is added to a collection

--- app/assets/javascripts/utils/changes.js
+++ app/assets/javascripts/utils/changes.js
@@ -7,6 +7,9 @@
 // should return false
 
 App.utils.onlyThoseAttrsChanged = (changedObj, arr) => {
+	if (_.isEmpty(changedObj)) { // changedObj is empty when a new object is added into a collection
+		return (false);
+	}
 	for (let key in changedObj) {
 		if (_.findIndex(arr, attr => attr == key) == -1) {
 			return (false); // some attribute not specified in arr changed
