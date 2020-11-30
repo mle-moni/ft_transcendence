dcdf70271eb1c722151c3a05edcda0dad9d8b200
qfeuilla <quentinfeuillademontixi42@gmail.com>
2020-12-01 00:46:42 +0100

    moving the endgame logic to de Game class

--- app/channels/play_channel.rb
+++ app/channels/play_channel.rb
@@ -5,41 +5,7 @@ class PlayChannel < ApplicationCable::Channel
 
   def unsubscribed
     if (params[:role] != "v")
-      room_name = "play_channel_#{params[:game_room_id]}";
-      # Any cleanup needed when channel is unsubscribed
-      if (Redis.current.get("game_#{room_name}_end?") == "no") # if not already ended, end the game with actual player has loser (he left the game)
-        Redis.current.set("game_#{room_name}_end?", "yes");
-
-        if (params[:role] == 'r')
-          current_user = User.find_by(email: Redis.current.get("#{room_name}_r"))
-          other_user = User.find_by(email: Redis.current.get("#{room_name}_l"))
-          current_score = $games[room_name][:right_score]
-          other_score = $games[room_name][:left_score]
-        else
-          current_user = User.find_by(email: Redis.current.get("#{room_name}_l"))
-          other_user = User.find_by(email: Redis.current.get("#{room_name}_r"))
-          current_score = $games[room_name][:left_score]
-          other_score = $games[room_name][:right_score]
-        end
-
-        if ($games[room_name][:is_ranked] == true)
-          match = EloRating::Match.new
-
-          match.add_player(rating: current_user.elo)
-          match.add_player(rating: other_user.elo, winner: true)
-          tmp = match.updated_ratings
-          current_user.elo = tmp[0]
-          other_user.elo = tmp[1]
-          if other_user.guild
-            other_user.guild.points += 1
-            other_user.guild.save
-          end
-          current_user.save
-          other_user.save
-        end
-        Match.create(winner: other_user, loser: current_user, winner_score: other_score, loser_score: current_score);
-        ActionCable.server.broadcast room_name, {action: 'quit'}
-      end
+      Game.end_the_game("play_channel_#{params[:game_room_id]}", params[:role])
     end
   end
 
@@ -81,39 +47,6 @@ class PlayChannel < ApplicationCable::Channel
   end
 
   def end_the_game(data)
-    if (Redis.current.get("game_#{data['room_name']}_end?") == "no") # if not already ended, end the game with the actual datas
-      Redis.current.set("game_#{data['room_name']}_end?", "yes");
-
-      if ($games[data['room_name']][:right_score] == 11)
-        winner_user = User.find_by(email: Redis.current.get("#{data['room_name']}_r"))
-        loser_user = User.find_by(email: Redis.current.get("#{data['room_name']}_l"))
-        winner_score = $games[data['room_name']][:right_score]
-        loser_score = $games[data['room_name']][:left_score]
-      else
-        winner_user = User.find_by(email: Redis.current.get("#{data['room_name']}_l"))
-        loser_user = User.find_by(email: Redis.current.get("#{data['room_name']}_r"))
-        winner_score = $games[data['room_name']][:left_score]
-        loser_score = $games[data['room_name']][:right_score]
-      end
-
-      if ($games[data['room_name']][:is_ranked] == true)
-        match = EloRating::Match.new
-
-        match.add_player(rating: loser_user.elo)
-        match.add_player(rating: winner_user.elo, winner: true)
-        tmp = match.updated_ratings
-        loser_user.elo = tmp[0]
-        winner_user.elo = tmp[1]
-        if winner_user.guild
-          winner_user.guild.points += 1
-          other_user.guild.save
-        end
-        loser_user.save
-        winner_user.save
-      end
-
-      Match.create(winner: winner_user, loser: loser_user, winner_score: winner_score, loser_score: loser_score);
-      ActionCable.server.broadcast data['room_name'], {action: 'quit'}
-    end
+    Game.end_the_game(data["room_name"], 'n')
   end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -30,4 +30,57 @@ class Game < ApplicationRecord
 		puts "Game started with data :"
 		puts room_name, is_ranked
 	end
+
+	def self.end_the_game(room_name, role_quit) # role_quit is 'r' or 'l' when someone gave up else 'n'
+		if (Redis.current.get("game_#{room_name}_end?") == "no") # if not already ended, end the game with actual player has loser (he left the game)
+			Redis.current.set("game_#{room_name}_end?", "yes");
+
+			puts "ok1"
+			if (role_quit != 'n')
+				if (role_quit == 'r')
+					loser_user = User.find_by(email: Redis.current.get("#{room_name}_r"))
+					winner_user = User.find_by(email: Redis.current.get("#{room_name}_l"))
+					loser_score = $games[room_name][:right_score]
+					winner_score = $games[room_name][:left_score]
+				else
+					loser_user = User.find_by(email: Redis.current.get("#{room_name}_l"))
+					winner_user = User.find_by(email: Redis.current.get("#{room_name}_r"))
+					loser_score = $games[room_name][:left_score]
+					winner_score = $games[room_name][:right_score]
+				end
+			else
+				if ($games[room_name][:right_score] == 11)
+					winner_user = User.find_by(email: Redis.current.get("#{room_name}_r"))
+					loser_user = User.find_by(email: Redis.current.get("#{room_name}_l"))
+					winner_score = $games[room_name][:right_score]
+					loser_score = $games[room_name][:left_score]
+				else
+					winner_user = User.find_by(email: Redis.current.get("#{room_name}_l"))
+					loser_user = User.find_by(email: Redis.current.get("#{room_name}_r"))
+					winner_score = $games[room_name][:left_score]
+					loser_score = $games[room_name][:right_score]
+				end
+			end
+			puts "ok2"
+
+			if ($games[room_name][:is_ranked] == true)
+				match = EloRating::Match.new
+				match.add_player(rating: loser_user.elo)
+				match.add_player(rating: winner_user.elo, winner: true)
+				tmp = match.updated_ratings
+				loser_user.elo = tmp[0]
+				winner_user.elo = tmp[1]
+				if winner_user.guild
+					winner_user.guild.points += 1
+					winner_user.guild.save
+				end
+				loser_user.save
+				winner_user.save
+			end
+			puts "ok3"
+
+			Match.create(winner: winner_user, loser: loser_user, winner_score: winner_score, loser_score: loser_score);
+			ActionCable.server.broadcast room_name, {action: 'quit'}
+		end
+	end
 end
