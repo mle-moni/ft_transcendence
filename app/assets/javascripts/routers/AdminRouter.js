d3e9cb45ba6d5be61f2cbf6dc1d7144872ad149c
HenriBrg <hnr.berger@gmail.com>
2020-12-06 20:58:22 +0100

    Deny URL Acces to Admin view if not global administrator

--- app/assets/javascripts/routers/AdminRouter.js
+++ app/assets/javascripts/routers/AdminRouter.js
@@ -5,6 +5,14 @@ AppClasses.Routers.AdminRouter = class extends AppClasses.Routers.AbstractRouter
 		this.route("admin", "index");
 	}
 	index() {
+		if (this.models.user) {
+			const isAdmin = this.models.user.attributes.admin;
+			if (!isAdmin) {
+				App.toast.alert("Unauthorized", { duration: 2000, style: App.toastStyle });
+				location.hash = "#";
+				return ;
+			}
+		}
 		this.basicView("admin", "Admin", {model: this.models.user});
 	}
 }
